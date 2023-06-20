06db89da6444e5f3b45e375939c8b9566313eea6
leobennett85 <leobennett85@mail.com>
2023-06-20 21:18:28 -0230

    Deconstructing MainWiggle

--- ReactPract/src/SVG/SvgMainWiggle.jsx
+++ ReactPract/src/SVG/SvgMainWiggle.jsx
@@ -1,9 +1,32 @@
-import MainWiggle from '../assets/svgMainWiggle.svg'
 const SvgMainWiggle = () => {
+    
+    const dMainWiggle  = "m 0,25 " +
+                         "c 0,0 " +
+                            "117,-6 " +
+                            "75,11 " +
+                            "-55,17 " +
+                            "65,2 " +
+                            "65,2"
 
+    
     return (
         <>
-            <img src={MainWiggle} />
+            <svg 
+                id="mainWiggle"
+                height="100%" 
+                width="100%" 
+                viewBox="0 0 100 100" 
+                preserveAspectRatio="none"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlns:svg="http://www.w3.org/2000/svg">
+                <path 
+                    id="mainWigglePath"
+                    fill="none"
+                    stroke="black"
+                    strokeWidth="0.07"
+                    d={dMainWiggle} />
+            </svg>               
+
         </>
     )
 }
