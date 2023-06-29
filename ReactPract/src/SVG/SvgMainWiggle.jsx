92843043a6c197136284ab2489147c52aedf3fc3
leobennett85 <leobennett85@mail.com>
2023-06-28 22:04:31 -0230

    Wiggle now dynamically created

--- ReactPract/src/SVG/SvgMainWiggle.jsx
+++ ReactPract/src/SVG/SvgMainWiggle.jsx
@@ -1,34 +1,32 @@
 const SvgMainWiggle = () => {
     
-    const dMainWiggle = "m 0,20 " +
-                        "c 40,15 " +
-                        "40,-15 " +
-                        "100,10";
-    
-    {/* Contructor function for creating wiggles */}
+    {/* Constructor function for creating wiggles */}
     const mOrigin =  [0,20];
     const c1Origin = [40,15];
     const c2Origin = [40,-15];
     const c3Origin = [100,10];
 
-    function Wiggle(m, c1, c2, c3) {
-        this.m = m;
-        this.c1 = c1;
-        this.c2 = c2;
-        this.c3 = c3;
+    function Wiggle(mOrigin, c1Origin, c2Origin, c3Origin) {
+        this.m = mOrigin;
+        this.c1 = c1Origin;
+        this.c2 = c2Origin;
+        this.c3 = c3Origin;
+    }
+
+    Wiggle.prototype.toString = function() {
+        return `${this.m} ${this.c1} ${this.c2} ${this.c3}`;
     }
 
     const wiggleTestObj = new Wiggle(
         "m " + mOrigin[0] + "," + mOrigin[1] + " ",
-        "c " + c1Origin[0] + "," + c2Origin[1] + " ",
+        "c " + c1Origin[0] + "," + c1Origin[1] + " ",
         c2Origin[0] + "," + c2Origin[1] + " ",
         c3Origin[0] + "," + c3Origin[1]
     );
+    
+    console.log(wiggleTestObj.toString());
 
-    const pathOriginString = JSON.stringify(Object.values(wiggleTestObj));
-
-
-    console.log(pathOriginString);
+    const pathOriginString = wiggleTestObj.toString();
 
 
                     
@@ -37,8 +35,8 @@ const SvgMainWiggle = () => {
         <>
             <svg 
                 id="mainWiggle"
-                height="100" 
-                width="100" 
+                height="100%" 
+                width="100%" 
                 viewBox="0 0 100 100" 
                 preserveAspectRatio="none"
                 xmlns="http://www.w3.org/2000/svg"
@@ -48,7 +46,7 @@ const SvgMainWiggle = () => {
                     fill="none"
                     stroke="black"
                     strokeWidth="0.07"
-                    d={dMainWiggle} />
+                    d={pathOriginString} />
             </svg>    
 
         </>
